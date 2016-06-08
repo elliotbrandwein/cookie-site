68d61731eff12acb859aa8bda4d41b4fbbc1f4df
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-08 13:08:32 -0400

    inital commit

---
+++ myCookies.js
@@ -0,0 +1,29 @@
+function cookieTest()
+{
+	console.log("pressed");
+	var name = document.getElementById("name").value;
+	if(Cookies.get(name)==undefined)
+	{
+		Cookies.set(name,name);
+		console.log("cookie made");
+	}
+	else
+	{
+		var x =("hello "+Cookies.get(name) );
+		document.getElementById("intro").innerHTML = x;
+		document.getElementById("clickMe").innerHTML="click for something fun";
+		setInterval(function(){ 
+			$('body').css ("background-color",getRandomColor());
+		}, 100);
+		
+	}
+
+}
+  function getRandomColor() {
+ var letters = '0123456789ABCDEF'.split('');
+ var color = '#';
+ for (var i = 0; i < 6; i++ ) {
+ color += letters[Math.floor(Math.random() * 16)];
+ }
+ return color;
+}
