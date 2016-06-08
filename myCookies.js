cea548bc183ddabc85b52fa3d78d48cb8feddd08
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-08 13:46:51 -0400

    fixed the seizure issue

--- myCookies.js
+++ myCookies.js
@@ -12,18 +12,23 @@ function cookieTest()
 		var x =("hello "+Cookies.get(name) );
 		document.getElementById("intro").innerHTML = x;
 		document.getElementById("clickMe").innerHTML="click for something fun";
-		setInterval(function(){ 
+		setInterval(function()
+		{ 
 			$('body').css ("background-color",getRandomColor());
 		}, 100);
-		
+		document.getElementById("clickMe").disabled=true;
 	}
 
 }
-  function getRandomColor() {
- var letters = '0123456789ABCDEF'.split('');
- var color = '#';
- for (var i = 0; i < 6; i++ ) {
- color += letters[Math.floor(Math.random() * 16)];
- }
- return color;
+ function getRandomColor()
+ {
+	var letters = '0123456789ABCDEF'.split('');
+	var color = '#';
+	for (var i = 0; i < 6; i++ )
+	{
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	x=false;
+	Cookies.set("hit",1);
+	return color;
 }
